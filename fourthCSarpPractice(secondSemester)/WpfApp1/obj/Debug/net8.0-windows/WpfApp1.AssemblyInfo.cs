162b98f7989d30c2c30cc0a1b9fbb1f9acab80c5
EtoNeAnanas <ananasovii.yashik@gmail.com>
2024-03-11 20:23:14 +0300

    add fourth practice

--- fourthCSarpPractice(secondSemester)/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
+++ fourthCSarpPractice(secondSemester)/WpfApp1/obj/Debug/net8.0-windows/WpfApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+637600cba4b6a02b4813e6c88709f05be992d4be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c012b0a1ddfa6b01188cd77f49efe8953b048ad0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
